41dff3afdb06b0ffe4ce47f776e234ddd9920077
Alejandro <alejogc2002@hotmail.com>
2025-04-17 22:24:42 -0500

    Se crea página ¿qué es? de cataratas y se elimina heart

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,13 @@ import Home from './pages/home/Home'
 import NotFound from './pages/not-found/NotFound'
 import AgujeroMacular from './pages/agujero-macular/AgujeroMacular'
 import Cataratas from './pages/cataratas/Cataratas'
+import QueEsCatarata from './pages/cataratas/que-es-catarata/QueEsCatarata'
 import Conjuntivitis from './pages/conjuntivitis/Conjuntivitis'
 import Glaucoma from './pages/glaucoma/Glaucoma'
 import Quiz from './pages/quiz/Quiz'
 import InicioSesion from './pages/inicio-sesion/InicioSesion'
-import Heart from './pages/heart/Heart'
-import HighBloodPressure from './pages/heart/high-blood-pressure/HighBloodPressure'
-import LowBloodPressure from './pages/heart/low-blood-pressure/LowBloodPressure'
 import Layout from './layout/Layout'
+
 
 
 createRoot(document.getElementById('root')).render(
@@ -22,16 +21,14 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route index path="/" element={<Home />} />  
         <Route path="agujero-macular" element={<AgujeroMacular />} /> 
-        <Route path="cataratas" element={<Cataratas />} /> 
+        <Route path="cataratas" element={<Cataratas />}>
+          <Route path="que-es" element={<QueEsCatarata />} />
+        </Route>
         <Route path="conjuntivitis" element={<Conjuntivitis />} /> 
         <Route path="glaucoma" element={<Glaucoma />} /> 
         <Route path="quiz" element={<Quiz />} /> 
         <Route path="inicio-sesion" element={<InicioSesion />} />
         <Route path="*" element={<NotFound />} />  
-        <Route path="corazon" element={<Heart/>} >  
-          <Route path="presion-alta" element={<HighBloodPressure />} /> 
-          <Route path="presion-baja" element={<LowBloodPressure />} /> 
-        </Route>
       </Routes>
     </Layout>
   </BrowserRouter>
