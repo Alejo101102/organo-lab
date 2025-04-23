1a7329c0fc2df3e46960f802f2302259f5b88516
heidy <heidy.gelpud@correounivalle.edu.co>
2025-04-23 00:17:25 -0500

    Actualizacion de modelo Glaucoma, cambio de letra en home

--- src/pages/glaucoma/que-es-glaucoma/QueEsGlaucoma.jsx
+++ src/pages/glaucoma/que-es-glaucoma/QueEsGlaucoma.jsx
@@ -1,9 +1,9 @@
-import Eyes from '../../home/models-3d/Eyes';
 import './QueEsGlaucoma.css';
 import { Link } from 'react-router';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import Footer from '../../../layout/footer/Footer';
+import EyeWithGlaucoma from '../models-3d/EyeWithGlaucoma';
 
 
 const QueEsGlaucoma = () => {
@@ -16,8 +16,14 @@ const QueEsGlaucoma = () => {
           <img src="/images/glaucoma/glaucoma-anatomico.png" alt="Anatomía de Glaucoma" className="img-cuadrada" />
          
           <div className="modelo-3d">
-            <Eyes />
+            <Canvas camera={{ position: [0, 0, 6], fov: 25 }}>
+            <ambientLight intensity={0.5} />
+            <directionalLight position={[2, 2, 2]} />
+            <OrbitControls enableZoom={true} autoRotate />
+            <EyeWithGlaucoma />
+           </Canvas>
           </div>
+
 
           <img src="/images/glaucoma/glaucoma-persona-1.png" alt="Glaucoma persona 1" className="img-cuadrada" />
           
@@ -26,7 +32,7 @@ const QueEsGlaucoma = () => {
   
         <div className="texto">
           <p>
-          El glaucoma es un grupo de enfermedades de los ojos que pueden causar pérdida de visión y ceguera al dañar el nervio ubicado en la
+          El **glaucoma es un grupo de enfermedades de los ojos que pueden causar pérdida de visión y ceguera al dañar el nervio ubicado en la
           conocido como nervio óptico. Con frecuencia, el daño al nervio óptico es causado por el aumento de la presión en el ojo. Esta se llama presión intraocular.
           </p>
           <p>
