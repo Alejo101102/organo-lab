a794fddfa0aed4e102a7a3c9fcf18427c04214a4
Alejandro <alejogc2002@hotmail.com>
2025-04-30 09:40:08 -0500

    Corrección de rutas

--- src/pages/glaucoma/Glaucoma.jsx
+++ src/pages/glaucoma/Glaucoma.jsx
@@ -2,11 +2,12 @@
 import React from 'react'
 import './Glaucoma.css'
 import QueEsGlaucoma from './que-es-glaucoma/QueEsGlaucoma'
+import { Outlet } from 'react-router-dom'
 
 const Glaucoma = () => {
   return (
     <div>
-      <QueEsGlaucoma />
+      <Outlet />
     </div>
   )
 }
