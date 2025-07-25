28ba88ae1221b4c9bcf64ae5eeafe671b289ec1a
heidy <heidy.gelpud@correounivalle.edu.co>
2025-07-25 17:38:44 -0500

    Avance: Diseño Medallero

--- src/pages/quiz/ResultadoJuego.jsx
+++ src/pages/quiz/ResultadoJuego.jsx
@@ -1,15 +1,25 @@
 // ResultadoJuego.jsx
 import React from 'react';
 import './ResultadoJuego.css';
+import { useNavigate } from 'react-router-dom';
+import Medallero from './Medallero'  
 
-export default function ResultadoJuego({ resultado, onClickPodio }) {
+export default function ResultadoJuego({ resultado }) {
+
+  const navigate = useNavigate();
+
+  const irAMedallero = () => {
+    navigate('/quiz/cuestionario/medallero');
+  };
+
+
   return (
     <div className="overlay-resultado">
       <div className="modal-resultado">
         <h2 className="titulo-resultado">
           {resultado === 'ganar' ? '¡Ganaste!' : 'Perdiste :('}
         </h2>
-        <button className="boton-resultado" onClick={onClickPodio}>
+        <button className="boton-resultado" onClick={irAMedallero}>
           Ir al podio
         </button>
       </div>
